2cb8b4e5d2630a1fbd91cc2860e86fdc13db91b8
Enabulelejerry <jerryenabulele@gmail.com>
2025-06-25 17:21:03 +0100

    add migrate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        //test
         \App\Models\User::factory()->create([
             'name' => 'Admin user',
             'email' => 'admin@example.com',
